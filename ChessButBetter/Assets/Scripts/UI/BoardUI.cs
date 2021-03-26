afd89017d877e3057b52cd21e59ad9bf9aa3d55c
Richard Ding <rmding2013college@gmail.com>
2021-03-25 21:30:03 -0700

    Add colliders to squares and get mouse input working

--- ChessButBetter/Assets/Scripts/UI/BoardUI.cs
+++ ChessButBetter/Assets/Scripts/UI/BoardUI.cs
@@ -10,17 +10,25 @@ namespace ChessButBetter
         public int squareSize;
         public int boardLength;
         public int boardWidth;
+        Camera cam;
         // Start is called before the first frame update
         void Start()
         {
+            cam = Camera.main;
             generateBoard();
         }
 
-        // // Update is called once per frame
-        // void Update()
-        // {
-
-        // }
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                if (hit.collider != null)
+                {
+                    Debug.Log("Hit " + hit.collider.gameObject.name);
+                }
+            }    
+        }
 
         void generateBoard()
         {
@@ -35,9 +43,12 @@ namespace ChessButBetter
                     // Instantiate is a shortcut to creating gameobjects if you have prefabs
                     // Without instantiate, you could create an object like GameObject obj = GameObject.CreatePrimitive(PrimitiveType.Quad) for a basic square object
                     GameObject square = Instantiate(squarePrefab, new Vector2(xPos, yPos), Quaternion.identity);
+                    square.name = row + ", " + col;
                     square.transform.localScale = new Vector3(squareSize, squareSize, 0);
                     squareRenderers[row, col] = square.GetComponent<SpriteRenderer>();
                     squareRenderers[row, col].color = Color.yellow;
+                    BoxCollider2D collider = square.GetComponent<BoxCollider2D>();
+                    collider.size = new Vector2(1, 1);
                     xPos += squareSize + 0.2f;
                 }
                 xPos = 0;
